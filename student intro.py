e7cc3ea124e25b64188599f3df076324e997d3c1
VidushiRaghuvanshi <vidushiraghuvanshi062@gmail.com>
2024-11-16 16:32:31 +0530

    student2

---
+++ student intro.py
@@ -0,0 +1,11 @@
+class student:
+    name="Vidushi"
+    grade="10th"
+    def stu(self):
+        print("Hi,I'm from India")
+    def details(self):
+        print("my name is",self.name)
+        print("I'm in",self.grade,"grade.")
+ob=student()
+ob.stu()
+ob.details()
